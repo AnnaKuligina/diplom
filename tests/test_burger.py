936d7f63699f287c3387cd344e5715d9eb5d0cb2
Anna Kuligina <anuta5561@mail.ru>
2025-07-18 13:33:11 +0300

    init

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -3,36 +3,33 @@ from unittest.mock import Mock
 
 
 class TestBurger:
+    def test_set_buns(self, burger, bun): # тест добавления булочки
+        burger.set_buns(bun)
+        assert burger.bun == bun
 
-    def test_set_buns(self, burger, bun_mock): # тест добавления булочки
-        burger.set_buns(bun_mock)
-        assert burger.bun == bun_mock
-
-    def test_add_ingredient(self, burger, sauce_mock): # тест добавления ингредиента в бургер
-        burger.add_ingredient(sauce_mock)
-        assert sauce_mock in burger.ingredients
+    def test_add_ingredient(self, burger, sauce): # тест добавления ингредиента в бургер
+        burger.add_ingredient(sauce)
+        assert sauce in burger.ingredients
         assert len(burger.ingredients) == 1
 
-    def test_remove_ingredient(self, burger, sauce_mock, filling_mock): # тест удаления ингредиента
-        burger.ingredients = [sauce_mock, filling_mock]
-        burger.remove_ingredient(0)
-        assert burger.ingredients.count(sauce_mock) == 0
-        assert burger.ingredients.count(filling_mock) == 1
+    def test_remove_ingredient(self, prepared_burger, sauce): # тест удаления ингредиента
+        initial_count = len(prepared_burger.ingredients)
+        prepared_burger.remove_ingredient(0)
+        assert len(prepared_burger.ingredients) == initial_count - 1
+        assert sauce not in prepared_burger.ingredients
 
-    def test_remove_invalid_index(self, burger, sauce_mock): # тест невалидных индексов при удалении ингредиента
+    def test_remove_invalid_index(self, burger, sauce): # тест невалидных индексов при удалении ингредиента
         with pytest.raises(IndexError):
             burger.remove_ingredient(0)
 
-        burger.add_ingredient(sauce_mock)
+        burger.add_ingredient(sauce)
         with pytest.raises(IndexError):
             burger.remove_ingredient(1)
 
-    def test_move_ingredient(self, burger, sauce_mock, filling_mock): # тест перемещения ингредиента в бургере
-        burger.add_ingredient(sauce_mock)
-        burger.add_ingredient(filling_mock)
-        burger.move_ingredient(0, 1)
-        assert burger.ingredients[0] == filling_mock
-        assert burger.ingredients[1] == sauce_mock
+    def test_move_ingredient(self, prepared_burger, sauce, filling): # тест перемещения ингредиента в бургере
+        prepared_burger.move_ingredient(0, 1)
+        assert prepared_burger.ingredients[0] == filling
+        assert prepared_burger.ingredients[1] == sauce
 
     @pytest.mark.parametrize("bun_price, ingredients_prices, expected", [
         (100, [], 200),
@@ -41,32 +38,24 @@ class TestBurger:
         (0, [0, 0], 0),
         (300, [150, 250, 100], 1100)
     ])
-    def test_get_price(self, burger, bun_mock, bun_price, ingredients_prices, expected): # тест расчета цены с использованием параметризации
-        bun_mock.get_price.return_value = bun_price
-        ingredients = []
+    def test_get_price(self, burger, bun, bun_price, ingredients_prices, expected): # тест расчета цены с использованием параметризации
+        bun.get_price.return_value = bun_price
+        burger.set_buns(bun)
+
         for price in ingredients_prices:
             ingredient = Mock()
             ingredient.get_price.return_value = price
-            ingredients.append(ingredient)
-
-        burger.set_buns(bun_mock)
-        for ingredient in ingredients:
             burger.add_ingredient(ingredient)
 
         assert burger.get_price() == expected
 
-    def test_get_receipt(self, burger, bun_mock, sauce_mock, filling_mock): # тест получения чека
-        burger.set_buns(bun_mock)
-        burger.add_ingredient(sauce_mock)
-        burger.add_ingredient(filling_mock)
-
+    def test_get_receipt(self, prepared_burger, bun, sauce, filling): # тест получения чека
         expected = (
-            "(==== black bun ====)\n"
-            "= sauce hot sauce =\n"
-            "= filling cutlet =\n"
-            "(==== black bun ====)\n"
+            f"(==== {bun.get_name()} ====)\n"
+            f"= {sauce.get_type().lower()} {sauce.get_name()} =\n"
+            f"= {filling.get_type().lower()} {filling.get_name()} =\n"
+            f"(==== {bun.get_name()} ====)\n"
             "\n"
-            "Price: 400"
+            f"Price: {400}"
         )
-        assert burger.get_receipt() == expected
-
+        assert prepared_burger.get_receipt() == expected
